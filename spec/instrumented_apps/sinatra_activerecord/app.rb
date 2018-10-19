f5116f4867173778d0493c8f20ae52fe032fc3d0
Sam Stokes <sam@honeycomb.io>
2018-10-19 16:26:44 -0500

    Add DEBUG=true to env to make tests more noisy

--- spec/instrumented_apps/sinatra_activerecord/app.rb
+++ spec/instrumented_apps/sinatra_activerecord/app.rb
@@ -7,8 +7,9 @@ require 'support/db_active_record'
 require 'support/fake_auth'
 require 'support/fakehoney'
 require 'support/only_one_app'
+require 'support/test_logger'
 
-Honeycomb.init service_name: 'sinatra_activerecord', client: $fakehoney
+Honeycomb.init service_name: 'sinatra_activerecord', client: $fakehoney, logger: $test_logger
 
 class SinatraActiveRecordApp < Sinatra::Base
   include ThereCanBeOnlyOneApp
