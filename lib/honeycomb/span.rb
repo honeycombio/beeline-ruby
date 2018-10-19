20ce671bbda6682dacbbc82339ae70fd70a07ade
Sam Stokes <sam@honeycomb.io>
2018-10-19 16:14:40 -0500

    Replace with_trace_id with with_trace_context

--- lib/honeycomb/span.rb
+++ lib/honeycomb/span.rb
@@ -1,11 +1,18 @@
+require 'base64'
+require 'json'
 require 'securerandom'
 
 module Honeycomb
   class << self
     # @api private
-    def with_trace_id(trace_id = SecureRandom.uuid)
+    def with_trace_context(encoded_context = nil)
+      trace_context = decode_trace_context(encoded_context) || {}
+      trace_id = trace_context.fetch(:trace_id, SecureRandom.uuid)
+      parent_span_id = trace_context[:parent_span_id]
+      context = trace_context.fetch(:context, {})
+
       Thread.current[:honeycomb_trace_id] = trace_id
-      yield trace_id
+      yield trace_id, parent_span_id, context
     ensure
       Thread.current[:honeycomb_trace_id] = nil
     end
@@ -51,6 +58,115 @@ module Honeycomb
         duration = finish - start
         event.add_field 'duration_ms', duration * 1000
         event.send
+      end
+    end
+
+    def decode_trace_context(encoded_context)
+      return nil unless encoded_context
+      version, payload = encoded_context.split(';', 2)
+      case version
+      when '1'
+        decode_payload_v1(payload)
+      else
+        nil
+      end
+    end
+
+    def encode_trace_context_v1(trace_id, parent_span_id, context)
+      version = 1
+
+      encoded_payload = encode_payload_v1(
+        trace_id: trace_id,
+        parent_id: parent_span_id,
+        context: context,
+      )
+
+      "#{version};#{encoded_payload}"
+    end
+    alias encode_trace_context encode_trace_context_v1
+
+    private
+    def decode_payload_v1(encoded_payload)
+      trace_id, parent_span_id, context = nil
+
+      encoded_payload.split(',').each do |entry|
+        k, v = entry.split('=', 2)
+        case k
+        when 'trace_id'
+          trace_id = v
+        when 'parent_id'
+          parent_span_id = v
+        when 'context'
+          context = decode_payload_context_v1(v)
+        else
+        end
+      end
+
+      if trace_id.nil?
+        return nil
+      elsif parent_span_id.nil?
+        return nil
+      end
+
+      payload = {
+        trace_id: trace_id,
+        parent_span_id: parent_span_id,
+      }
+      payload[:context] = context if context
+      payload
+    rescue StandardError => e
+      nil
+    end
+
+    def decode_payload_context_v1(encoded_payload_context)
+      return {} if encoded_payload_context.empty?
+      json = Base64.decode64(encoded_payload_context)
+      JSON.parse(json)
+    end
+
+    def encode_payload_v1(payload_parts)
+      payload_parts.map do |k, v|
+        encoded_part = encode_payload_part_v1(k, v)
+        encoded_part ? "#{k}=#{encoded_part}" : nil
+      end
+        .compact # strip out parts that failed to encode
+        .join(',')
+    end
+
+    def encode_payload_part_v1(param, value)
+      case param
+      when :trace_id, :parent_id
+        encode_payload_id_v1(value)
+      when :context
+        encode_payload_context_v1(value)
+      end
+    end
+
+    def encode_payload_id_v1(id)
+      case id
+      when nil
+        nil
+      when String, Symbol
+        id = id.to_s
+        if id.include? ','
+          raise ArgumentError, "can't include ','"
+        end
+        id
+      when Numeric
+        id.to_s
+      else
+        raise ArgumentError, "invalid type #{id.class}"
+      end
+    end
+
+    def encode_payload_context_v1(context)
+      case context
+      when nil
+        nil
+      when Hash
+        Base64.urlsafe_encode64(JSON.generate(context)).strip
+      else
+        raise ArgumentError, "invalid type #{context.class}"
       end
     end
   end
