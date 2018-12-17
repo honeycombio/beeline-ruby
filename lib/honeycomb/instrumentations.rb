c2e22e14cb8e4586db87b13d1b0585b68f8de8fc
Martin Holman <me@martinholman.co.nz>
2018-12-17 18:30:51 +1300

    Change how auto_install plugins are added
    By using strings to identify the plugins we can alter the sequel plugin
    to only define it's modules if required. We need this as defining the
    Sequel constant is causing issues with other gems

--- lib/honeycomb/instrumentations.rb
+++ lib/honeycomb/instrumentations.rb
@@ -7,20 +7,24 @@ require 'sequel-honeycomb/auto_install'
 
 module Honeycomb
   INSTRUMENTATIONS = [
-    ActiveRecord::Honeycomb,
-    Faraday::Honeycomb,
-    Rack::Honeycomb,
-    Sequel::Honeycomb,
+    "ActiveRecord",
+    "Faraday",
+    "Rack",
+    "Sequel",
   ].freeze
 
   INSTRUMENTATIONS.each do |instrumentation|
-    auto = instrumentation::AutoInstall
-    hook_label = instrumentation.name.sub(/::Honeycomb$/, '').downcase.to_sym
-    after_init(hook_label) do |client, logger|
-      if auto.available?(logger: logger)
-        auto.auto_install!(honeycomb_client: client, logger: logger)
-      else
-        logger.debug "Not autoinitialising #{instrumentation.name}" if logger
+    instrumentation_name = "#{instrumentation}::Honeycomb"
+    if const_defined? instrumentation_name
+      instrumentation = Object.const_get instrumentation_name
+      auto = instrumentation::AutoInstall
+      hook_label = instrumentation.name.sub(/::Honeycomb$/, '').downcase.to_sym
+      after_init(hook_label) do |client, logger|
+        if auto.available?(logger: logger)
+          auto.auto_install!(honeycomb_client: client, logger: logger)
+        else
+          logger.debug "Not autoinitialising #{instrumentation.name}" if logger
+        end
       end
     end
   end
