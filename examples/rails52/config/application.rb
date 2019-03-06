f2d4f954d5e23a55addb24dd3ee27e0faf94fa72
Martin Holman <me@martinholman.co.nz>
2019-03-06 11:44:58 -0800

    Add honeycomb to sample rails52 application

--- examples/rails52/config/application.rb
+++ examples/rails52/config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Honeycomb
-  class Application < Rails::Application
+  class Application < ::Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
--- examples/rails52/config/environments/development.rb
+++ examples/rails52/config/environments/development.rb
@@ -58,4 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.honeycomb.write_key = "{WRITE_KEY}"
+  config.honeycomb.dataset = "rails"
+  config.honeycomb.service_name = "rails_development"
 end
