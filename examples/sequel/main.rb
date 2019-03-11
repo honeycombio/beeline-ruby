6503880dce4c70e0afba0bc61477731d3a8156b2
Martin Holman <me@martinholman.co.nz>
2019-03-11 15:10:24 -0700

    Add sequel example

---
+++ examples/sequel/main.rb
@@ -0,0 +1,32 @@
+require "sequel"
+require "honeycomb-beeline"
+
+Honeycomb.configure do |config|
+  config.write_key = "write_key"
+  config.dataset = "dataset"
+  config.service_name = "service_name"
+end
+
+# connect to an in-memory database
+DB = Sequel.sqlite
+
+# create an items table
+DB.create_table :items do
+  primary_key :id
+  String :name, unique: true, null: false
+  Float :price, null: false
+end
+
+# create a dataset from the items table
+items = DB[:items]
+
+# populate the table
+items.insert(name: 'abc', price: rand * 100)
+items.insert(name: 'def', price: rand * 100)
+items.insert(name: 'ghi', price: rand * 100)
+
+# print out the number of records
+puts "Item count: #{items.count}"
+
+# print out the average price
+puts "The average price is: #{items.avg(:price)}"
--- lib/honeycomb/integrations/sequel.rb
+++ lib/honeycomb/integrations/sequel.rb
@@ -5,9 +5,17 @@ require "sequel"
 module Honeycomb
   # Wrap sequel commands in a span
   module Sequel
-    attr_accessor :honeycomb_client
+    def honeycomb_client
+      @honeycomb_client || Honeycomb.client
+    end
+
+    def honeycomb_client=(client)
+      @honeycomb_client = client
+    end
 
     def log_connection_yield(sql, conn, args = nil)
+      return super if honeycomb_client.nil?
+
       honeycomb_client.start_span(name: sql.sub(/\s+.*/, "").upcase) do |span|
         span.add_field "meta.package", "sequel"
         span.add_field "meta.package_version", ::Sequel::VERSION
@@ -20,3 +28,4 @@ module Honeycomb
 end
 
 Sequel::Database.register_extension(:honeycomb, Honeycomb::Sequel)
+Sequel::Database.extension :honeycomb
