e0c75aa880f8e0e4474d60231f0e6d6808cf8e7b
Martin Holman <me@martinholman.co.nz>
2019-03-14 17:26:35 -0700

    'Refactor' rack code

--- lib/honeycomb/integrations/rack.rb
+++ lib/honeycomb/integrations/rack.rb
@@ -18,6 +18,17 @@ module Honeycomb
       ["sinatra.route", "request.route"],
     ].freeze
 
+    COMMON_USER_FIELDS = %i[
+      email
+      name
+      first_name
+      last_name
+      created_at
+      id
+    ].freeze
+
+    SCOPE_PATTERN = /^warden\.user\.([^.]+)\.key$/.freeze
+
     attr_reader :app, :client
 
     def initialize(app, client:)
@@ -28,34 +39,91 @@ module Honeycomb
     def call(env)
       hny = env["HTTP_X_HONEYCOMB_TRACE"]
       client.start_span(name: "http_request", serialized_trace: hny) do |span|
-        add_env_field = lambda do |(env_key, key)|
-          env_value = env[env_key]
-          next unless env_value && !env_value.empty?
+        add_field = lambda do |key, value|
+          next unless value && !value.empty?
 
-          span.add_field(key, env_value)
+          span.add_field(key, value)
         end
 
-        if defined?(::Rails::VERSION::STRING)
-          span.add_field("meta.package", "rails")
-          span.add_field("meta.package_version", ::Rails::VERSION::STRING)
-        elsif defined?(::Sinatra::VERSION)
-          span.add_field("meta.package", "sinatra")
-          span.add_field("meta.package_version", ::Sinatra::VERSION)
-        elsif defined?(::Rack::VERSION)
-          span.add_field("meta.package", "rack")
-          span.add_field("meta.package_version", ::Rack::VERSION.join("."))
-        end
-
-        RACK_FIELDS.each(&add_env_field)
+        add_package_information(&add_field)
+        extract_fields(env, RACK_FIELDS, &add_field)
+        extract_rails_controller_action(env, &add_field)
+        extract_rails_route(env, &add_field)
 
         status, headers, body = app.call(env)
 
-        # this is populated after the action is executed
-        SINATRA_FIELDS.each(&add_env_field)
+        extract_user_information(env, &add_field)
+        extract_fields(env, SINATRA_FIELDS, &add_field)
 
         span.add_field("response.status_code", status)
 
         [status, headers, body]
+      end
+    end
+
+    def add_package_information
+      if defined?(::Rails::VERSION::STRING)
+        yield "meta.package", "rails"
+        yield "meta.package_version", ::Rails::VERSION::STRING
+      elsif defined?(::Sinatra::VERSION)
+        yield "meta.package", "sinatra"
+        yield "meta.package_version", ::Sinatra::VERSION
+      elsif defined?(::Rack::VERSION)
+        yield "meta.package", "rack"
+        yield "meta.package_version", ::Rack::VERSION.join(".")
+      end
+    end
+
+    def extract_fields(env, fields)
+      fields.each do |key, value|
+        yield value, env[key]
+      end
+    end
+
+    def extract_rails_controller_action(env)
+      rails_params = env["action_dispatch.request.parameters"]
+
+      return unless rails_params && rails_params.is_a?(Hash)
+
+      yield "request.controller", rails_params["controller"]
+      yield "request.action", rails_params["action"]
+    end
+
+    def extract_rails_route(env)
+      routes = env["action_dispatch.routes"]
+
+      return unless routes
+
+      request = ::Rack::Request.new(env)
+      found_route = false
+      routes.router.recognize(request) do |route, _|
+        break if found_route
+
+        found_route = true
+        yield "request.route", "#{env['REQUEST_METHOD']} #{route.path.spec}"
+      end
+    end
+
+    def extract_user_information(env)
+      warden = env["warden"]
+
+      return unless warden
+
+      session = env["rack.session"] || {}
+      keys = session.keys.select do |key|
+        key.match(SCOPE_PATTERN)
+      end
+      warden_scopes = keys.map do |key|
+        key.gsub(SCOPE_PATTERN, "\1")
+      end
+      best_scope = warden_scopes.include?("user") ? "user" : warden_scopes.first
+
+      return unless best_scope
+
+      env["warden"].user(scope: best_scope, run_callbacks: false).tap do |user|
+        COMMON_USER_FIELDS.each do |field|
+          user.respond_to?(field) && yield("user.#{field}", user.send(field))
+        end
       end
     end
   end
