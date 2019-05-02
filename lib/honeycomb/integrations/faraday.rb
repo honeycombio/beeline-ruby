9ed5518ef919c64df21e8431d563a3aa3654e16b
Martin Holman <me@martinholman.co.nz>
2019-05-02 10:20:41 -0700

    Consistency with go-beeline

--- lib/honeycomb/integrations/faraday.rb
+++ lib/honeycomb/integrations/faraday.rb
@@ -11,12 +11,12 @@ module Honeycomb
     end
 
     def call(env)
-      @client.start_span(name: nil) do |span|
-        span.add_field("type", "http_client")
+      @client.start_span(name: "http_client") do |span|
         span.add_field "request.method", env.method.upcase
         span.add_field "request.protocol", env.url.scheme
         span.add_field "request.host", env.url.host
         span.add_field "request.path", env.url.path
+        span.add_field "meta.type", "http_client"
         span.add_field "meta.package", "faraday"
         span.add_field "meta.package_version", ::Faraday::VERSION
 
