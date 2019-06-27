172fdd8ad9e53973798effdeb68a8bd8bcd27b6b
Martin Holman <me@martinholman.co.nz>
2019-06-26 17:45:41 -0700

    Insert middleware more defensively
    Run this after the standard file based initializers have been run so that the honeycomb client should have been configured. Also only insert the middleware if the client has been configured

--- lib/honeycomb/integrations/rails.rb
+++ lib/honeycomb/integrations/rails.rb
@@ -31,13 +31,15 @@ module Honeycomb
 
   # Automatically capture rack requests and create a trace
   class Railtie < ::Rails::Railtie
-    initializer "honeycomb.install_middleware" do |app|
-      # what location should we insert the middleware at?
-      app.config.middleware.insert_before(
-        ::Rails::Rack::Logger,
-        Honeycomb::Rack,
-        client: Honeycomb.client,
-      )
+    initializer "honeycomb.install_middleware", after: :load_config_initializers do |app|
+      if Honeycomb.client
+        # what location should we insert the middleware at?
+        app.config.middleware.insert_before(
+          ::Rails::Rack::Logger,
+          Honeycomb::Rack,
+          client: Honeycomb.client,
+        )
+      end
     end
   end
 end
