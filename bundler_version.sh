b2c142dd6360990f0def35f27d462d49a5902c09
Martin Holman <me@martinholman.co.nz>
2019-06-27 14:23:41 -0700

    Add ruby 2.6 testing

--- bundler_version.sh
+++ bundler_version.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-set -uex
+set -ux
 
 if [[ "$BUNDLE_GEMFILE" =~ (rails_41.gemfile|rails_42.gemfile)$ ]]; then
-  if [[ "$TRAVIS_RUBY_VERSION" =~ ^(2.3)$ ]]; then
-    gem uninstall -v '>= 2' -i $(rvm gemdir) -ax bundler
-  elif [[ "$TRAVIS_RUBY_VERSION" =~ ^(2.4|2.5)$ ]]; then
-    gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler
-  fi
+  gem uninstall -v '>= 2' -i $(rvm gemdir) -ax bundler
+  gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler
   gem install bundler -v '< 2'
 fi
