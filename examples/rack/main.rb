80908e4d23aef2a0d133e0db0277972093b43e4b
Martin Holman <me@martinholman.co.nz>
2019-07-09 14:57:48 -0700

    Add rack example

---
+++ examples/rack/main.rb
@@ -0,0 +1,26 @@
+require "rack"
+require "honeycomb-beeline"
+
+Honeycomb.configure do |config|
+  config.write_key = "write_key"
+  config.dataset = "dataset"
+  config.service_name = "service_name"
+  config.client = Libhoney::LogClient.new
+end
+
+handler = Rack::Handler::WEBrick
+
+class RackApp
+  def call(env)
+    Honeycomb.start_span(name: "inner span") do
+      [200, {"Content-Type" => "text/plain"}, ["Hello from Honeycomb"]]
+    end
+  end
+end
+
+app = Rack::Builder.new do |builder|
+  builder.use Honeycomb::Rack, client: Honeycomb.client
+  builder.run RackApp.new
+end
+
+handler.run app
