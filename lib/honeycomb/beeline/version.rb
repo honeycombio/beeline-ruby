a0998861164dc7951860f0d35921563b49127cb3
Martin Holman <me@martinholman.co.nz>
2019-07-23 14:23:55 -0700

    v1.0.0

--- lib/honeycomb/beeline/version.rb
+++ lib/honeycomb/beeline/version.rb
@@ -3,7 +3,7 @@
 module Honeycomb
   module Beeline
     NAME = "honeycomb-beeline".freeze
-    VERSION = "1.0.0-beta4".freeze
+    VERSION = "1.0.0".freeze
     USER_AGENT_SUFFIX = "#{NAME}/#{VERSION}".freeze
   end
 end
