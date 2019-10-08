84841ae75b9dfc2b0a8fce7da2d441ca93b3fd1f
Martin Holman <me@martinholman.co.nz>
2019-10-08 11:41:57 -0700

    Remove travis

--- bundler_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ux
-
-if [[ "$BUNDLE_GEMFILE" =~ (rails_41.gemfile|rails_42.gemfile)$ ]]; then
-  gem uninstall -v '>= 2' -i $(rvm gemdir) -ax bundler
-  gem uninstall -v '>= 2' -i $(rvm gemdir)@global -ax bundler
-  gem install bundler -v '< 2'
-fi
