645078b92f021fcfbf8b46070cb790bf27385bd1
Martin Holman <martin308@gmail.com>
2020-03-05 15:56:05 -0800

    Add some more basic checks

--- spec/generators/honeycomb/honeycomb_generator_spec.rb
+++ spec/generators/honeycomb/honeycomb_generator_spec.rb
@@ -5,14 +5,41 @@ if defined?(Honeycomb::Rails)
 
   RSpec.describe HoneycombGenerator do
     describe "simple execution" do
-      it "creates the initializer file" do
+      let(:writekey) { "generator_write_key" }
+      let(:default_dataset) { "rails" }
+      let(:dataset) { "generator_dataset" }
+      let(:init_file) { File.join(@dir, "config/initializers/honeycomb.rb") }
+
+      around(:example) do |example|
         Dir.mktmpdir do |dir|
-          Dir.chdir(dir) do
-            Rails::Generators.invoke("honeycomb", ["writekey"])
-            init_file = File.join(dir, "config/initializers/honeycomb.rb")
-            expect(File.exist?(init_file)).to eq(true)
+          Dir.chdir dir do
+            @dir = dir
+            example.run
           end
         end
+      end
+
+      it "creates the initializer file" do
+        Rails::Generators.invoke("honeycomb", [writekey])
+        expect(File.exist?(init_file)).to eq(true)
+      end
+
+      it "sets the writekey correctly" do
+        Rails::Generators.invoke("honeycomb", [writekey])
+        require init_file
+        expect(Honeycomb.client.libhoney.writekey).to eq(writekey)
+      end
+
+      it "sets the dataset to the default" do
+        Rails::Generators.invoke("honeycomb", [writekey])
+        require init_file
+        expect(Honeycomb.client.libhoney.dataset).to eq(default_dataset)
+      end
+
+      it "sets the dataset correctly" do
+        Rails::Generators.invoke("honeycomb", [writekey, "--dataset", dataset])
+        require init_file
+        expect(Honeycomb.client.libhoney.dataset).to eq(dataset)
       end
     end
   end
