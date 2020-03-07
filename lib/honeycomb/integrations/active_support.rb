3892c704c39fe9a2f2d3a5541d3112fd845a1f4c
Liz Fong-Jones <lizf@honeycomb.io>
2020-03-06 20:56:05 -0500

    Serializes ActionController::Parameters correctly
    Don't prematurely call to_s on hashes, as we can actually serialize them in a way we can parse.
    
    This enables us to construct nested JSON in libhoney, serializing things so they can be later unpacked on Honeycomb's side.

--- lib/honeycomb/integrations/active_support.rb
+++ lib/honeycomb/integrations/active_support.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "active_support/notifications"
+require "libhoney/cleaner"
 
 module Honeycomb
   module ActiveSupport
@@ -8,6 +9,8 @@ module Honeycomb
     # Included in the configuration object to specify events that should be
     # subscribed to
     module Configuration
+      include Libhoney::Cleaner
+
       attr_accessor :notification_events
 
       def after_initialize(client)
@@ -34,7 +37,8 @@ module Honeycomb
           on_notification_event.call(name, span, payload)
         else
           payload.each do |key, value|
-            span.add_field("#{name}.#{key}", value.to_s)
+            value = value.to_hash if value.respond_to?(:to_hash)
+            span.add_field("#{name}.#{key}", clean_data(value))
           end
         end
       end
