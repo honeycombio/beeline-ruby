bcb785d6b5d9a9c215e68f97a0a6239019f81b44
Mike Goldsmith <goldsmith.mike@gmail.com>
2020-11-06 15:51:59 +0000

    prepare 2.3.0 release (#122)

--- lib/honeycomb/beeline/version.rb
+++ lib/honeycomb/beeline/version.rb
@@ -3,7 +3,7 @@
 module Honeycomb
   module Beeline
     NAME = "honeycomb-beeline".freeze
-    VERSION = "2.2.0".freeze
+    VERSION = "2.3.0".freeze
     USER_AGENT_SUFFIX = "#{NAME}/#{VERSION}".freeze
   end
 end
