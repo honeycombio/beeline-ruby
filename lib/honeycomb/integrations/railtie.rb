fa7b55bc7bbc4aa1bb31dc491c573a9d9a45e6ca
Jon <jonathon.p.anderson@hey.com>
2020-11-10 14:50:32 -0800

    Move Middleware to be before ActionDispatch::ShowExceptions (#123)
    This all came out of a very long conversation in the Honeycomb Pollinators slack. A user with a fresh Rails app seemed to be getting `response.status_code` set to `nil` on any errors in production. In local development it seemed to work, though this confusion went back and forth. We went back and forth setting a lot of config options, and eventually realized that unhandled exceptions were making it to the `Honeycomb::Rack::Middleware`. These would hit at the [`app.call`](https://github.com/honeycombio/beeline-ruby/blob/main/lib/honeycomb/integrations/rack.rb#L50) section and invoke the [`Honeycomb::Client#start_span`'s rescue block](https://github.com/honeycombio/beeline-ruby/blob/main/lib/honeycomb/client.rb#L71), set `error` and `error_detail`, and bypass the status code setting. 
    
    The reason for this eventually turns out that [`ActionDispatch::ShowExceptions`](https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/middleware/show_exceptions.rb#L35), so long as `config.action_dispatch.show_exceptions` is either set to `true` or `nil` (which it is by default in all environments except `test`), then it sets the rack app response to be the error code and body. Similarly _in `development` only_, [`ActionDispatch::DebugExceptions`](https://github.com/rails/rails/blob/master/actionpack/lib/action_dispatch/middleware/debug_exceptions.rb#L60) (technically anywhere that sets `config.action_dispatch.consider_all_requests_local = true`, but by default that's only `development`), will also rescue errors and render a fancy page. If that config is set to `false`, however, as it is in production, the error is just re-raised.
    
    By placing Honeycomb _below_ `ShowExceptions` in the stack, the only default chance we have of rescuing errors and rendering an error response is `DebugExceptions`. That middleware, however, only does so for "local" requests (e.g., in development), and does nothing in production except log an error and re-raise. Placing the Honeycomb Middleware _above_ `ShowExceptions`, though, allows the error response to be set and Honeycomb to accurately send along the status code.
    
    This issue can be replicated in at least the `rails52` example in this repo:
    
    1. Configure the `honeycomb.rb` write key
    2. I like to edit the `BeesController#index` to be something like `Bee.cant_beelieve_this_method`
    3. `bundle exec rails s`
    4. Visit localhost:3000/bees a few times then look at honeycomb, grouping by `response.status_code`. They should all be set to 500. The page visited from the rails server should be the giant orange Rails error stack trace page.
    5. Set `config.consider_all_requests_local` to `false` in `development.rb` then restart the server.
    6. Visit localhost:3000/bees a few times then check Honeycomb, grouping by `response.status_code`. The will all be blank; the error page should be the "Something went wrong..." rails default page.
    
    You can repeat the above with this patch and in _both_ cases you will get a `response.status_code` appropriately set.

--- lib/honeycomb/integrations/railtie.rb
+++ lib/honeycomb/integrations/railtie.rb
@@ -6,15 +6,17 @@ require "honeycomb/integrations/rails"
 module Honeycomb
   # Automatically capture rack requests and create a trace
   class Railtie < ::Rails::Railtie
+    def self.insert_middleware(app, client)
+      app.config.middleware.insert_before(
+        ActionDispatch::ShowExceptions,
+        Honeycomb::Rails::Middleware,
+        client: client,
+      )
+    end
+
     initializer("honeycomb.install_middleware",
                 after: :load_config_initializers) do |app|
-      if Honeycomb.client
-        app.config.middleware.insert_after(
-          ActionDispatch::ShowExceptions,
-          Honeycomb::Rails::Middleware,
-          client: Honeycomb.client,
-        )
-      end
+      self.class.insert_middleware(app, Honeycomb.client) if Honeycomb.client
     end
   end
 end
