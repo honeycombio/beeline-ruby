0e99ecc0a834927a753c30cc1b2d9c3f3d8b18a8
Robb Kidd <robbkidd@honeycomb.io>
2021-09-14 18:29:26 -0400

    remove CodeCov from test suite (#172)
    Also remove the main Gemfile.lock to allow gem versions to float to
    latest resolveable by gemspec and Ruby version constraints. Because
    Gemfile.lock has been removed to allow dependencies to resolve to
    latest possible, we'll cache dependencies retrieved based on changes
    to the gemspec instead of the lock file. Good 'nuff.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
-SUPPORT_CODECOV = Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.4")
-
 require "bundler/setup"
 require "simplecov"
 require "simplecov-console"
-require "codecov" if SUPPORT_CODECOV
 require "webmock/rspec"
 require "pry"
 
@@ -13,7 +10,6 @@ WebMock.disable_net_connect!
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 SimpleCov.formatter = SimpleCov::Formatter::Console
-SimpleCov.formatter = SimpleCov::Formatter::Codecov if SUPPORT_CODECOV
 
 # Make coverage work with Appraisals
 SimpleCov.command_name(ENV["BUNDLE_GEMFILE"].split.last || "")
