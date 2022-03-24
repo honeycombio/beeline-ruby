honeycombio/beeline-ruby

commit 7e10dcbb5cc612c6d4283c870d2b7084170c8288

remove children after sending (#98)
Resolves #84.

The spec changes look really bad, but the main change is to dump the existing specs into another `describe` block and add an additional spec to check that after the child is sent it won't try to send any sent children. Might be easiest to just look at the last two commits as the shuffling around is isolated to the first commit.

lib/honeycomb/span.rb
@@ -36,11 +36,16 @@ module Honeycomb
       parse_options(**options)
     end
 
-    def parse_options(parent_id: nil,
+    def parse_options(parent: nil,
+                      parent_id: nil,
                       is_root: parent_id.nil?,
                       sample_hook: nil,
                       presend_hook: nil,
                       **_options)
+      @parent = parent
+      # parent_id should be removed in the next major version bump. It has been
+      # replaced with passing the actual parent in. This is kept for backwards
+      # compatability
       @parent_id = parent_id
       @is_root = is_root
       @presend_hook = presend_hook
@@ -51,6 +56,7 @@ module Honeycomb
       self.class.new(trace: trace,
                      builder: builder,
                      context: context,
+                     parent: self,
                      parent_id: id,
                      sample_hook: sample_hook,
                      presend_hook: presend_hook).tap do |c|
@@ -73,9 +79,14 @@ module Honeycomb
       send_internal
     end
 
+    def remove_child(child)
+      children.delete child
+    end
+
     private
 
     attr_reader :event,
+                :parent,
                 :parent_id,
                 :children,
                 :builder,
@@ -92,14 +103,7 @@ module Honeycomb
     end
 
     def send_internal
-      add_field "duration_ms", duration_ms
-      add_field "trace.trace_id", trace.id
-      add_field "trace.span_id", id
-      add_field "meta.span_type", span_type
-      parent_id && add_field("trace.parent_id", parent_id)
-      add rollup_fields
-      add trace.fields
-      span_type == "root" && add(trace.rollup_fields)
+      add_additional_fields
       send_children
       sample = true
       if sample_hook.nil?
@@ -114,6 +118,19 @@ module Honeycomb
       end
       @sent = true
       context.span_sent(self)
+
+      parent && parent.remove_child(self)
+    end
+
+    def add_additional_fields
+      add_field "duration_ms", duration_ms
+      add_field "trace.trace_id", trace.id
+      add_field "trace.span_id", id
+      add_field "meta.span_type", span_type
+      parent_id && add_field("trace.parent_id", parent_id)
+      add rollup_fields
+      add trace.fields
+      span_type == "root" && add(trace.rollup_fields)
     end
 
     def send_children

.rubocop.yml
@@ -29,6 +29,9 @@ Metrics/LineLength:
   Exclude:
     - spec/support/event_data_shared_examples.rb
 
+Metrics/ParameterLists:
+  Max: 6
+
 Style/FrozenStringLiteralComment:
   EnforcedStyle: always
   Exclude:

spec/honeycomb/span_spec.rb
@@ -24,86 +24,107 @@ end
 
 RSpec.describe Honeycomb::Span do
   let(:libhoney_client) { Libhoney::TestClient.new }
-  let(:presend_hook) { nil }
-  let(:sample_rate) { 1 }
-  let(:sample_hook) do
-    lambda do |_fields|
-      [sampling_decision, sample_rate]
-    end
-  end
   let(:context) { Honeycomb::Context.new }
   let(:builder) { libhoney_client.builder }
   let(:trace) { Honeycomb::Trace.new(builder: builder, context: context) }
-  subject(:span) do
-    Honeycomb::Span.new(trace: trace,
-                        builder: builder,
-                        context: context,
-                        sample_hook: sample_hook,
-                        presend_hook: presend_hook)
-  end
 
-  describe "when the sampling hook returns false" do
-    let(:sampling_decision) { false }
+  describe "sample_hook and presend_hook behaviour" do
+    let(:presend_hook) { nil }
+    let(:sample_rate) { 1 }
+    let(:sample_hook) do
+      lambda do |_fields|
+        [sampling_decision, sample_rate]
+      end
+    end
 
-    it "does not send the event" do
-      expect { span.send }.not_to(change { libhoney_client.events })
+    subject(:span) do
+      Honeycomb::Span.new(trace: trace,
+                          builder: builder,
+                          context: context,
+                          sample_hook: sample_hook,
+                          presend_hook: presend_hook)
+    end
+
+    describe "when the sampling hook returns false" do
+      let(:sampling_decision) { false }
+
+      it "does not send the event" do
+        expect { span.send }.not_to(change { libhoney_client.events })
+      end
+    end
+
+    describe "when a span creates a child span" do
+      let(:presend_hook) { double("PresendHook") }
+      let(:sample_hook) { double("SampleHook") }
+
+      before do
+        allow(presend_hook).to receive(:call)
+        # we have to configure the sample_hook here to return the expected value
+        # as the presend_hook will not be called if the event is not going to be
+        # sent
+        allow(sample_hook).to receive(:call).and_return([true, 0])
+      end
+
+      it "sets the sample_hook on the child" do
+        expect(sample_hook).to receive(:call)
+          .with(hash_including("honeycomb" => "bees"))
+
+        child = span.create_child
+        child.add_field("honeycomb", "bees")
+        child.send
+      end
+
+      it "sets the presend_hook on the child" do
+        expect(presend_hook).to receive(:call)
+          .with(hash_including("honeycomb" => "bees"))
+
+        child = span.create_child
+        child.add_field("honeycomb", "bees")
+        child.send
+      end
+    end
+
+    describe "when the sampling hook returns true" do
+      let(:presend_hook) { double("PresendHook") }
+      let(:sampling_decision) { true }
+      let(:sample_rate) { 10 }
+
+      it "sends the event" do
+        allow(presend_hook).to receive(:call)
+        expect { span.send }.to change { libhoney_client.events.count }.by(1)
+      end
+
+      it "calls the presend hook" do
+        expect(presend_hook).to receive(:call)
+          .with(hash_including("honeycomb" => "bees"))
+
+        span.add_field("honeycomb", "bees")
+        span.send
+      end
+
+      it "sets the correct sample rate on the event" do
+        allow(presend_hook).to receive(:call)
+        span.send
+        expect(libhoney_client.events)
+          .to all(have_attributes(sample_rate: sample_rate))
+      end
     end
   end
 
-  describe "when a span creates a child span" do
-    let(:presend_hook) { double("PresendHook") }
-    let(:sample_hook) { double("SampleHook") }
-
-    before do
-      allow(presend_hook).to receive(:call)
-      # we have to configure the sample_hook here to return the expected value
-      # as the presend_hook will not be called if the event is not going to be
-      # sent
-      allow(sample_hook).to receive(:call).and_return([true, 0])
+  describe "sending children and parents" do
+    let(:parent) do
+      Honeycomb::Span.new(trace: trace,
+                          builder: builder,
+                          context: context)
     end
 
-    it "sets the sample_hook on the child" do
-      expect(sample_hook).to receive(:call)
-        .with(hash_including("honeycomb" => "bees"))
+    subject(:child) { parent.create_child }
 
-      child = span.create_child
-      child.add_field("honeycomb", "bees")
+    it "will remove itself from it's parent after being sent" do
+      expect(child).not_to receive(:send_by_parent)
+
       child.send
-    end
-
-    it "sets the presend_hook on the child" do
-      expect(presend_hook).to receive(:call)
-        .with(hash_including("honeycomb" => "bees"))
-
-      child = span.create_child
-      child.add_field("honeycomb", "bees")
-      child.send
-    end
-  end
-
-  describe "when the sampling hook returns true" do
-    let(:presend_hook) { double("PresendHook") }
-    let(:sampling_decision) { true }
-    let(:sample_rate) { 10 }
-
-    it "sends the event" do
-      allow(presend_hook).to receive(:call)
-      expect { span.send }.to change { libhoney_client.events.count }.by(1)
-    end
-
-    it "calls the presend hook" do
-      expect(presend_hook).to receive(:call)
-        .with(hash_including("honeycomb" => "bees"))
-
-      span.add_field("honeycomb", "bees")
-      span.send
-    end
-
-    it "sets the correct sample rate on the event" do
-      allow(presend_hook).to receive(:call)
-      span.send
-      expect(libhoney_client.events)
-        .to all(have_attributes(sample_rate: sample_rate))
+      parent.send
     end
   end
 end

commit c1404391e2b0be99387c67a72f71f9cb1565ecd4

Fix regression with mid vs leaf meta.span_type detection (#194)
A regression was introduced by #98 in computing the `meta.span_type` field.

The field's value is given by the `Honeycomb::Span#span_type` method: https://github.com/honeycombio/beeline-ruby/blob/8b36cd0c94aba9378b6b0d74c9b593fc3f2bac0e/lib/honeycomb/span.rb#L180-L188

This field gets added as the span is being sent: https://github.com/honeycombio/beeline-ruby/blob/8b36cd0c94aba9378b6b0d74c9b593fc3f2bac0e/lib/honeycomb/span.rb#L159

For "mid" spans, this amounts to checking the `Honeycomb::Span#children` array at send-time. Prior to #98, this array would be full all the time, as no values were ever removed. However, #98 added this line at the end of `#send_internal`: https://github.com/honeycombio/beeline-ruby/blob/8b36cd0c94aba9378b6b0d74c9b593fc3f2bac0e/lib/honeycomb/span.rb#L152

So if all of a mid span's children are sent before the mid (which is typical of synchronous traces), its `#children` array will be empty at send-time, yielding an erroneous `#span_type` of "leaf".

This is captured by running the new regression tests from this PR against the old code:

```console
$ rspec spec/honeycomb/client_spec.rb:324 spec/honeycomb/client_spec.rb:350
Run options: include {:locations=>{"./spec/honeycomb/client_spec.rb"=>[324, 350]}}

Honeycomb::Client
  meta.span_type
    when children are sent before parents
      has the right values (FAILED - 1)
    when parents are sent before children
      has the right values

Failures:

  1) Honeycomb::Client meta.span_type when children are sent before parents has the right values
     Failure/Error: expect(mid["meta.span_type"]).to eq("mid")

       expected: "mid"
            got: "leaf"

       (compared using ==)
     # ./spec/honeycomb/client_spec.rb:342:in `block (5 levels) in <top (required)>'
     # ./spec/honeycomb/client_spec.rb:337:in `block (4 levels) in <top (required)>'

lib/honeycomb/span.rb
@@ -50,6 +50,7 @@ module Honeycomb
       # compatability
       @parent_id = parent_id
       @is_root = is_root
+      @is_leaf = true
     end
 
     def parse_hooks(sample_hook: nil,
@@ -71,6 +72,7 @@ module Honeycomb
                      presend_hook: presend_hook,
                      propagation_hook: propagation_hook).tap do |c|
         children << c
+        @is_leaf = false
       end
     end
 
@@ -132,6 +134,10 @@ module Honeycomb
       @is_root
     end
 
+    def leaf?
+      @is_leaf
+    end
+
     def send_internal
       add_additional_fields
       send_children
@@ -180,7 +186,7 @@ module Honeycomb
     def span_type
       if root?
         parent_id.nil? ? "root" : "subroot"
-      elsif children.empty?
+      elsif leaf?
         "leaf"
       else
         "mid"

spec/honeycomb/client_spec.rb
@@ -313,4 +313,97 @@ RSpec.describe Honeycomb::Client do
       end.to_not raise_error
     end
   end
+
+  describe "meta.span_type" do
+    let(:event_data) { libhoney_client.events.map(&:data) }
+
+    context "when children are sent before parents" do
+      before do
+        client.start_span(name: "root") do
+          client.start_span(name: "mid") do
+            client.start_span(name: "leaf") do
+              :ok
+            end
+          end
+        end
+      end
+
+      it "has the right values" do
+        leaf, mid, root = event_data
+        aggregate_failures do
+          expect(leaf["name"]).to eq("leaf")
+          expect(leaf["meta.span_type"]).to eq("leaf")
+
+          expect(mid["name"]).to eq("mid")
+          expect(mid["meta.span_type"]).to eq("mid")
+
+          expect(root["name"]).to eq("root")
+          expect(root["meta.span_type"]).to eq("root")
+        end
+      end
+    end
+
+    context "when parents are sent before children" do
+      before do
+        root = client.start_span(name: "root")
+        client.start_span(name: "mid")
+        client.start_span(name: "leaf")
+        root.send
+      end
+
+      it "has the right values" do
+        leaf, mid, root = event_data
+        aggregate_failures do
+          expect(leaf["name"]).to eq("leaf")
+          expect(leaf["meta.span_type"]).to eq("leaf")
+
+          expect(mid["name"]).to eq("mid")
+          expect(mid["meta.span_type"]).to eq("mid")
+
+          expect(root["name"]).to eq("root")
+          expect(root["meta.span_type"]).to eq("root")
+        end
+      end
+    end
+
+    context "when continuing a distributed trace" do
+      let(:upstream_configuration) do
+        Honeycomb::Configuration.new.tap do |config|
+          config.client = Libhoney::NullClient.new
+        end
+      end
+
+      let(:upstream_client) do
+        Honeycomb::Client.new(configuration: upstream_configuration)
+      end
+
+      before do
+        root = upstream_client.start_span(name: "root")
+        root.send
+
+        header = root.to_trace_header
+        client.start_span(name: "subroot", serialized_trace: header) do
+          client.start_span(name: "mid") do
+            client.start_span(name: "leaf") do
+              :ok
+            end
+          end
+        end
+      end
+
+      it "has the right values" do
+        leaf, mid, subroot = event_data
+        aggregate_failures do
+          expect(leaf["name"]).to eq("leaf")
+          expect(leaf["meta.span_type"]).to eq("leaf")
+
+          expect(mid["name"]).to eq("mid")
+          expect(mid["meta.span_type"]).to eq("mid")
+
+          expect(subroot["name"]).to eq("subroot")
+          expect(subroot["meta.span_type"]).to eq("subroot")
+        end
+      end
+    end
+  end
 end