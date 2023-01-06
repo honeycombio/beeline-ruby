047bee53b8a126a3ff14064bae872fa714f86479
JamieDanielson <jamieedanielson@gmail.com>
2023-01-06 15:55:38 -0500

    use proper env var in setup script

--- .circleci/setup-rubygems.sh
+++ .circleci/setup-rubygems.sh
@@ -1,3 +1,3 @@
 mkdir ~/.gem
-echo -e "---\r\n:rubygems_api_key: $GEM_HOST_API_KEY" > ~/.gem/credentials
+echo -e "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
 chmod 0600 /home/circleci/.gem/credentials
