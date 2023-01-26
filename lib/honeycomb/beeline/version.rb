7317a7922448a59b5f911f90304cef7a97c70604
JamieDanielson <jamieedanielson@gmail.com>
2023-01-26 17:22:42 -0500

    Revert "v2.11.1-dev"
    This reverts commit a10f72546de96197684da7f37d38c2f291c4788b.

--- lib/honeycomb/beeline/version.rb
+++ lib/honeycomb/beeline/version.rb
@@ -3,7 +3,7 @@
 module Honeycomb
   module Beeline
     NAME = "honeycomb-beeline".freeze
-    VERSION = "2.11.1-dev".freeze
+    VERSION = "2.11.0".freeze
     USER_AGENT_SUFFIX = "#{NAME}/#{VERSION}".freeze
   end
 end
